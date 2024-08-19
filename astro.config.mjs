d68529b19ae8cde877fcbb55298327e192d92489
Miriael <ravafiel@protonmail.com>
2024-08-20 01:59:18 +0200

    Refacfor for cloudflare deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Eliana-2.github.io',
-  base: '/bookstore-site'
+  site: 'https://bookstore-site.pages.dev',
+  base: ''
 });
